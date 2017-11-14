c810600517abc45a52088bfdaf5ecea6b95b4ff0
Sergiu Postolachi <sergiu.postolachi@endava.com>
2017-11-14 10:47:27 +0200

    boolean values logical operators program structure file

---
+++ eloquent/c0_operations.js
@@ -0,0 +1,27 @@
+
+console.log(" This is the first line \n And this is the s e c o n d "); // two lines
+
+console.log(" A n e w l i n e c h a r a c t e r is w r i t t e n like \"\\ n \"."); // single line
+
+
+console.log (typeof 4.5); // number
+console.log(typeof 'a'); // string
+console.log(-(10-2));
+
+
+// boolean values
+console.log("------------boolean values---------")
+console.log(3>2); // true
+console.log("abcdefg"<"abcdefgh"); // true
+console.log("Itchy" !="something"); // true
+
+console.log(NaN ==NaN); //false
+
+// logical operators
+console.log("------------logical operators---------")
+console.log(false && true); //false
+console.log(false || true); //true
+
+console.log(true ? 1:2);
+console.log(false ? 1:2);
+console.log(null == undefined);
